69ca2660b1137e551fa3bd1f79dd7221e465fff1
Evelyn <ljiaying@wustl.edu>
2026-08-26 16:52:56 -0500

    Assignment 0

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -30,6 +30,8 @@ public class RobotInstructions  {
 		 * as you desire.
 		 */
 		robot.forward(60);
+		robot.
+		robot.forward(60);
 		robot.turnLeft(90);
 		robot.forward(23);
 		robot.turnRight(90);
